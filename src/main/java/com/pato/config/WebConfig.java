b342c0bcaf06d069a85b30fc0a0251037525227d
pato15-7 <pato.pettini@hotmail.com>
2025-10-25 04:54:24 -0300

    cors arreglado

--- src/main/java/com/pato/config/WebConfig.java
+++ src/main/java/com/pato/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://itchy-mara-patriciopettini-f9bc694c.koyeb.app")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
